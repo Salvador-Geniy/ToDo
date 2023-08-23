b76e16e1b5073ee97d9426c1a707c5052d34ede6
Alexey <alexderendyaev@ya.ru>
2023-08-23 16:47:51 -0300

    Initial commit

---
+++ src/tasks_app/models.py
@@ -0,0 +1,16 @@
+from django.contrib.auth.models import User
+from django.db import models
+
+
+class Task(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    title = models.CharField(max_length=100)
+    description = models.TextField(null=True, blank=True)
+    is_complete = models.BooleanField(default=False)
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return self.title
+
+    class Meta:
+        ordering = ['is_complete', '-created_at']
