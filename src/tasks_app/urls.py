ee2849c2e8864e6a6b80f959b31f00d0f9324e22
Alexey <alexderendyaev@ya.ru>
2023-08-24 16:00:53 -0300

    CreateView, UpdateView, DetailView and DeleteView

--- src/tasks_app/urls.py
+++ src/tasks_app/urls.py
@@ -1,11 +1,13 @@
 from django.urls import path
 
-from tasks_app.views import TaskListView, TaskDetailView, TaskCreateView
+from tasks_app.views import (TaskListView, TaskDetailView,
+                             TaskCreateView, TaskUpdateView, TaskDeleteView)
 
 urlpatterns = [
     path('', TaskListView.as_view(), name='tasks'),
     path('task-detail/<int:pk>/', TaskDetailView.as_view(), name='task-detail'),
     path('task-create/', TaskCreateView.as_view(), name='task-create'),
-
+    path('task-update/<int:pk>/', TaskUpdateView.as_view(), name='task-update'),
+    path('task-delete/<int:pk>/', TaskDeleteView.as_view(), name='task-delete'),
 
 ]
--- src/tasks_app/views.py
+++ src/tasks_app/views.py
@@ -1,13 +1,14 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
+from django.shortcuts import redirect
 from django.urls import reverse_lazy
-from django.views.generic import ListView, DetailView, CreateView
+from django.views.generic import ListView, DetailView, CreateView, UpdateView, DeleteView
 
 from tasks_app.models import Task
 
 
 class TaskListView(LoginRequiredMixin, ListView):
     model = Task
-    template_name = 'tasks/task_list.html'
+    template_name = 'tasks_app/task_list.html'
     context_object_name = 'tasks'
 
     def get_context_data(self, *, object_list=None, **kwargs):
@@ -31,3 +32,29 @@ class TaskCreateView(LoginRequiredMixin, CreateView):
     def form_valid(self, form):
         form.instance.user = self.request.user
         return super(TaskCreateView, self).form_valid(form)
+
+
+class TaskUpdateView(LoginRequiredMixin, UpdateView):
+    model = Task
+    fields = ['title', 'description', 'is_complete']
+    success_url = reverse_lazy('tasks')
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if not request.user.is_superuser and self.object.user != request.user:
+            return redirect(reverse_lazy('tasks'))
+        return super().get(request, *args, **kwargs)
+
+
+class TaskDeleteView(LoginRequiredMixin, DeleteView):
+    model = Task
+    context_object_name = 'task'
+    template_name_suffix = '_delete'
+    success_url = reverse_lazy('tasks')
+
+    def get(self, request, *args, **kwargs):
+        self.object = self.get_object()
+        if not request.user.is_superuser and self.object.user != request.user:
+            return redirect(reverse_lazy('tasks'))
+        context = self.get_context_data(object=self.object)
+        return self.render_to_response(context)
